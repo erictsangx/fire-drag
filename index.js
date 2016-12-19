6bf8bd455da4d129d0533730be1da77dc0481253
erictsangx <erictsangx@gmail.com>
2016-12-19 13:47:57 +0800

    v2.0, prepare for migrating to web-extension sdk

--- index.js
+++ index.js
@@ -4,47 +4,6 @@
 
 'use strict';
 
-const self = require('sdk/self');
-const pageMods = require('sdk/page-mod');
-const tabs = require('sdk/tabs');
-const prefs = require('sdk/simple-prefs').prefs;
-const lowLevelAPI = require('./modules/lowLevelAPI');
-const data = self.data;
+console.log("hello world");
 
-require('./modules/preference');
-
-pageMods.PageMod({
-    include: ['*'],
-    contentScriptFile: data.url('dropHandler.js'),
-    contentScriptWhen: 'start',
-    onAttach: startListening
-});
-
-
-function startListening(worker) {
-    worker.port.on('triggerDrop', function (msg) {
-        if (msg.distance >= prefs.threshold) {
-            if (msg.search) {
-                let searchLink = lowLevelAPI.getSearchLink(msg.content);
-                tabs.open({
-                    url: searchLink,
-                    inBackground: prefs.searchIn,
-                    onOpen: onOpen
-                });
-            }
-            else {
-                let tab = tabs.open({
-                    url: msg.content,
-                    inBackground: prefs.openIn,
-                    onOpen: onOpen
-                });
-            }
-        }
-    });
-}
-
-function onOpen(tab) {
-    if (prefs.insertAfterCurrent) {
-        tab.index = tabs.activeTab.index + 1;
-    }
-}
+document.body.style.border = "5px solid green";
